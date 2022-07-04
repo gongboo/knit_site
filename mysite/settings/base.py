064105d406b204ffb5d47a143b77facc9d09516a
gongboo <53379511+gongboo@users.noreply.github.com>
2022-07-04 22:30:59 +0900

    init added

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -89,15 +89,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 # DATABASES = my_settings.DATABASES
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'USER': '',
-        # 'PASSWORD': ''
-    }
-}
+
 
 
 # Password validation
