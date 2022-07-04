eb8cfe3acf309bde46b83555c5187bff255d3edd
gongboo <53379511+gongboo@users.noreply.github.com>
2022-07-04 22:05:42 +0900

    Update prod.py

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -9,7 +9,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'USER': '',
-        # 'PASSWORD': ''
+        'USER': '',
+        'PASSWORD': ''
     }
 }
