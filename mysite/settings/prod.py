5af686eaedac2b967d7765e6c10a2092ef8ca4f5
gongboo <53379511+gongboo@users.noreply.github.com>
2022-07-04 21:41:57 +0900

    settings update
    db change

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -2,3 +2,13 @@
 from .base import *
 
 ALLOWED_HOSTS = [".ap-northeast-2.compute.amazonaws.com"] #고정 아이피 넣으랬는데 고정 아이피는 공개해도 되는 건지 몰라서 일단 전체 공개함
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',
+        'PASSWORD': ''
+    }
+}
