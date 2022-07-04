11a53d40d1793a1139f10234211a09c8234e8b07
gongboo <53379511+gongboo@users.noreply.github.com>
2022-07-04 22:04:44 +0900

    Update prod.py

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -7,8 +7,9 @@ ALLOWED_HOSTS = [".ap-northeast-2.compute.amazonaws.com"] #고정 아이피 넣�
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': '',
-        'PASSWORD': ''
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'USER': '',
+        # 'PASSWORD': ''
     }
 }
