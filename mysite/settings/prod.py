df415304538089caf42e3d3e6091e38a09548f78
gongboo <53379511+gongboo@users.noreply.github.com>
2022-07-28 17:55:42 +0900

    Update prod.py

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -1,7 +1,10 @@
 #production setting
 from .base import *
 
-ALLOWED_HOSTS = [".ap-northeast-2.compute.amazonaws.com"] #고정 아이피 넣으랬는데 고정 아이피는 공개해도 되는 건지 몰라서 일단 전체 공개함
+ALLOWED_HOSTS = [
+    ".ap-northeast-2.compute.amazonaws.com",
+    ".knitvibe.com"
+    ] 
 
 
 DATABASES = {
